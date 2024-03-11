6d7408626fd4f4e5e35c411321f3798c2e38502f
snipes <schneidsmc@gmail.com>
2024-03-10 19:57:48 -0500

    models... maybe

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,48 @@
 // export all the models
 
 // User
+const User = require('./models/user');
+
 // post
+const Post = require('./models/post');
+
 // comment
+const Comment = require('./models/comment');
 
 // association between models
     // user-post
+    User.hasMany(Post, {
+        foreignKey: 'user_id'
+    });
     // post-user
+    Post.belongsTo(User, {
+        foreignKey: 'user_id'
+    });
     // comment-user
+    Comment.belongsTo(User, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE',
+        hooks: true
+    });
     // comment-post
+    Comment.belongsTo(Post, {
+        foreignKey: 'post_id',
+        onDelete: 'CASCADE',
+        hooks: true
+    });
     // user-comment
+    User.hasMany(Comment, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE',
+        hooks: true
+    });
     // post-comment
+    Post.hasMany(Comment, {
+        foreignKey: 'post_id',
+        onDelete: 'CASCADE',
+        hooks: true
+    });
 
 
 // export
+module.exports = { User, Post, Comment };
